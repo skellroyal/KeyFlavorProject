6943b09e15d435191e019f1522ec8e061d8de612
Doris Liao <skellroyal@hotmail.com>
2015-11-19 13:57:03 +0800

    Edit files

--- KeyFlavorProject/settings.py
+++ KeyFlavorProject/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'trips',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -55,18 +56,25 @@ ROOT_URLCONF = 'KeyFlavorProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
+        'DIRS': [os.path.join(BASE_DIR, 'templates').replace('\\', '/')],
         'OPTIONS': {
             'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
+                'django.template.context_processors.debug',
+                'django.template.context_processors.i18n',
+                'django.template.context_processors.media',
+                'django.template.context_processors.static',
+                'django.template.context_processors.tz',
                 'django.contrib.messages.context_processors.messages',
             ],
+            'loaders': (
+                 'django.template.loaders.filesystem.Loader',
+                 'django.template.loaders.app_directories.Loader',
+            ),
         },
     },
 ]
+
 
 WSGI_APPLICATION = 'KeyFlavorProject.wsgi.application'
 
--- KeyFlavorProject/urls.py
+++ KeyFlavorProject/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+# import view functions from trips app
+#from trips.views import hello_world
+from trips.views import hello_world, home, post_detail
 
 urlpatterns = [
-    url(r'^accounts/', include('accounts.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^hello/$', hello_world),
+    url(r'^$', home),
+    url(r'^post/(?P<id>\d+)/$', post_detail, name='post_detail'),
 ]
